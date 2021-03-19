c950dbbc5135d8a1e4ab007fe427dd5b67b1291a
李浩 <iversonwool@sina.com>
2021-03-20 07:45:33 +0800

    Umi playground

---
+++ UmiApp/src/app.ts
@@ -0,0 +1,7 @@
+// import {history} from 'umi'
+
+// export function render(oldRender: Function) {
+//   console.log('------', location.pathname)
+//   history.replace('./hello')
+//   oldRender()
+// }
