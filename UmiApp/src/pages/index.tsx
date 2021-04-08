30ed9e7510c6a2fe884cdbf273304967d679004f
LeeHow <iversonwool@126.com>
2021-04-08 17:57:01 +0800

    TSgeneric

--- UmiApp/src/pages/index.tsx
+++ UmiApp/src/pages/index.tsx
@@ -2,6 +2,25 @@ import React from 'react';
 import styles from './index.less';
 
 export default () => {
+  // ts playground
+  const d: object = {
+    a: ''
+  }
+  d;
+
+  function identity<T>(arg: T): T {
+    return arg
+  }
+
+
+  function a<T>(arg: T[]): T[] {
+    return arg
+  }
+
+  const b: number[] = [1, 2, 3]
+
+  identity(2)
+
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
