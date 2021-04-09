69b9c7021425dd1a068cbf7541b3ce000d9baee5
LeeHow <iversonwool@126.com>
2021-04-09 09:17:37 +0800

    ts type generic

--- UmiApp/src/pages/index.tsx
+++ UmiApp/src/pages/index.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 import styles from './index.less';
 
+//generic types
+interface GenericFunc {
+  <Q>(arg: Q): Q
+}
+
+// 提升
+interface GenericFuncAll<W> {
+  (arg: W): W
+}
+
+//generic class
+class GenericClass<T> {
+  one: number = 0;
+  add: (a: T, b: T) => T = (a, b) => a
+}
+
+//extends
+//keyof
+
+
 export default () => {
+
+  const instance = new GenericClass<number>();
+  instance.one = 1;
+  instance.add = (a, b) => a + b;
+
+
   // ts playground
   const d: object = {
     a: ''
@@ -18,6 +44,11 @@ export default () => {
   }
 
   const b: number[] = [1, 2, 3]
+
+  let func: <T>(arg: T) => T = identity
+
+  let func1: { <S>(arg: S): S } = identity
+  let func2: GenericFunc = identity
 
   identity(2)
 
