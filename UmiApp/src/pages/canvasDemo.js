aff8b52a8dffecb7f9fd46717f35e5aaae82d881
Lee_Klaus <iversonwoool@gmail.com>
2023-07-21 22:02:39 +0800

    Update canvasDemo.js

--- UmiApp/src/pages/canvasDemo.js
+++ UmiApp/src/pages/canvasDemo.js
@@ -2,16 +2,19 @@ import React from 'react'
 import * as THREE from 'three'
 
 function CanvasDemo() {
-
+  
   function init() {
     const canvas = document.querySelector('#c')
     const renderer = new THREE.WebGLRenderer({ antialias: true, canvas })
 
 
     const scene = new THREE.Scene()
+    scene.background = new THREE.Color(0xaaaaaa)
 
     const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshPhongMaterial({ color: 'green' })
+    const material = new THREE.MeshPhongMaterial({ color: 0xffffff, side: THREE.DoubleSide })
+    const hue = Math.random()
+    material.color.setHSL(hue, 1, .5)
     const cube = new THREE.Mesh(geometry, material)
 
     scene.add(cube)
@@ -20,8 +23,21 @@ function CanvasDemo() {
     light.position.set(-1, 2, 4)
     scene.add(light)
 
-    const camera = new THREE.PerspectiveCamera(75, 2, .1, 5)
-    camera.position.z = 2
+    const camera = new THREE.PerspectiveCamera(75, 2, .1, 8)
+    camera.position.z = 3
+
+    function resizeRendererToDisplaySize(r) {
+      const c = r.domElement
+      const pixelRatio = window.devicePixelRatio
+
+      const width = c.clientWidth * pixelRatio
+      const height = c.clientHeight * pixelRatio 
+      const needResize = c.width !== width || c.height !== height
+      if (needResize) {
+        r.setSize(width, height, false)
+      }
+      return needResize
+    }
 
     function animate(time) {
 
@@ -29,6 +45,14 @@ function CanvasDemo() {
 
       cube.rotation.x = second
       cube.rotation.y = second
+
+      if (resizeRendererToDisplaySize(renderer)) {
+        const canvas = renderer.domElement
+        camera.aspect = canvas.clientWidth / canvas.clientHeight
+
+        camera.updateProjectionMatrix()
+      }
+
       renderer.render(scene, camera)
 
       requestAnimationFrame(animate)
@@ -43,7 +67,7 @@ function CanvasDemo() {
   }, [])
 
   return (
-    <canvas id="c" style={{width: 750, height: 375}}>CanvasDemo</canvas>
+    <canvas id="c" style={{width: '100%', height: '100vh', display: 'block'}}>CanvasDemo</canvas>
   )
 }
 
