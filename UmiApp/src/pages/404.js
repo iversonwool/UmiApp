2a5d924a33f1a523fbaac1d0d829f9e48884de8a
Lee_Klaus <iversonwoool@gmail.com>
2023-08-16 15:45:35 +0800

    js解释线程与渲染线程互斥

--- UmiApp/src/pages/404.js
+++ UmiApp/src/pages/404.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
+function requestData() {
+  const now = Date.now();
+
+  while(Date.now() - now <= 3 * 1000) {};
+
+  return 'after componentDidMount data';
+}
+
 class NotFound extends Component {
+  state = {
+    data: 'init data'
+  }
+
+  componentDidMount() {
+    this.setState({
+      data: requestData()
+    })
+  }
+
   render() {
+    const {data} = this.state
     return (
       <div>
         404, Not Found.
+        <div>{data}</div>
       </div>
     );
   }
