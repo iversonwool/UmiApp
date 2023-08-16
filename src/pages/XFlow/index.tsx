ca81e79cc667dd6671a1194a74835e9a313af755
Lee_Klaus <iversonwoool@gmail.com>
2023-08-16 17:10:51 +0800

    fix: 需要一个容器高度

--- src/pages/XFlow/index.tsx
+++ src/pages/XFlow/index.tsx
@@ -74,46 +74,49 @@ export const Demo: React.FC<IProps> = props => {
   }, [cache.app, meta])
 
   return (
-    <XFlow
-      className="dag-user-custom-clz"
-      hookConfig={graphHooksConfig}
-      modelServiceConfig={modelServiceConfig}
-      commandConfig={cmdConfig}
-      onLoad={onLoad}
-      meta={meta}
-    >
-      <DagGraphExtension />
-      <NodeCollapsePanel
-        className="xflow-node-panel"
-        searchService={dndPanelConfig.searchService}
-        nodeDataService={dndPanelConfig.nodeDataService}
-        onNodeDrop={dndPanelConfig.onNodeDrop}
-        position={{ width: 230, top: 0, bottom: 0, left: 0 }}
-        footerPosition={{ height: 0 }}
-        bodyPosition={{ top: 40, bottom: 0, left: 0 }}
-      />
-      <CanvasToolbar
-        className="xflow-workspace-toolbar-top"
-        layout="horizontal"
-        config={toolbarConfig}
-        position={{ top: 0, left: 230, right: 290, bottom: 0 }}
-      />
-      <XFlowCanvas position={{ top: 40, left: 230, right: 290, bottom: 0 }}>
-        <CanvasScaleToolbar position={{ top: 12, right: 12 }} />
-        <CanvasContextMenu config={menuConfig} />
-        <CanvasSnapline color="#faad14" />
-        <CanvasNodePortTooltip />
-      </XFlowCanvas>
-      <JsonSchemaForm
-        controlMapService={controlMapService}
-        formSchemaService={formSchemaService}
-        formValueUpdateService={formValueUpdateService}
-        bodyPosition={{ top: 0, bottom: 0, right: 0 }}
-        position={{ width: 290, top: 0, bottom: 0, right: 0 }}
-        footerPosition={{ height: 0 }}
-      />
-      <KeyBindings config={keybindingConfig} />
-    </XFlow>
+    <div style={{height: '100vh'}}>
+      <XFlow
+        className="dag-user-custom-clz"
+        hookConfig={graphHooksConfig}
+        modelServiceConfig={modelServiceConfig}
+        commandConfig={cmdConfig}
+        onLoad={onLoad}
+        meta={meta}
+      >
+        <DagGraphExtension />
+        <NodeCollapsePanel
+          className="xflow-node-panel"
+          searchService={dndPanelConfig.searchService}
+          nodeDataService={dndPanelConfig.nodeDataService}
+          onNodeDrop={dndPanelConfig.onNodeDrop}
+          position={{ width: 230, top: 0, bottom: 0, left: 0 }}
+          footerPosition={{ height: 0 }}
+          bodyPosition={{ top: 40, bottom: 0, left: 0 }}
+        />
+        <CanvasToolbar
+          className="xflow-workspace-toolbar-top"
+          layout="horizontal"
+          config={toolbarConfig}
+          position={{ top: 0, left: 230, right: 290, bottom: 0 }}
+        />
+        <XFlowCanvas position={{ top: 40, left: 230, right: 290, bottom: 0 }}>
+          <CanvasScaleToolbar position={{ top: 12, right: 12 }} />
+          <CanvasContextMenu config={menuConfig} />
+          <CanvasSnapline color="#faad14" />
+          <CanvasNodePortTooltip />
+        </XFlowCanvas>
+        <JsonSchemaForm
+          controlMapService={controlMapService}
+          formSchemaService={formSchemaService}
+          formValueUpdateService={formValueUpdateService}
+          bodyPosition={{ top: 0, bottom: 0, right: 0 }}
+          position={{ width: 290, top: 0, bottom: 0, right: 0 }}
+          footerPosition={{ height: 0 }}
+        />
+        <KeyBindings config={keybindingConfig} />
+      </XFlow>
+    </div>
+    
   )
 }
 
