1838ffc3d63b37400e16da12368601ce7436b857
Lee_Klaus <iversonwoool@gmail.com>
2024-04-02 16:45:37 +0800

    decorator

--- src/components/decorator.js
+++ src/components/decorator.js
@@ -6,24 +6,58 @@ class Person {
 
   @readonly
   sayHi() {
-    console.log(`hi ${this.name}`)
+    console.log(`hi ${this.name}`);
   }
 }
 
-
 function testable(isTestable) {
-
   return function (t, context) {
     console.log('context', context);
     t.isTestable = isTestable;
-  }
+  };
 }
 
 function readonly(v, name, descriptor) {
   descriptor.writable = false;
-  return descriptor
-
+  return descriptor;
 }
 
+export default Person;
 
-export default Person
+// function logged(value, context) {
+//   console.log(context)
+//   const {kind, name} = context
+//   if (kind === 'class') {
+//     return class extends value {
+//       constructor(...args) {
+//         super(...args);
+//         console.log(`constructing an instance of ${name} with arguments ${args.join(', ')}`);
+//       }
+//     }
+//   }
+// }
+
+// @logged
+// class C {}
+
+// new C();
+
+function replaceMethod(method) {
+  return function () {
+    return `How are you, ${this.name}`;
+  };
+}
+
+class Per {
+  constructor(name) {
+    this.name = name;
+  }
+
+  @replaceMethod
+  hello() {
+    return `Hi ${this.name}`;
+  }
+}
+
+const lucy = new Per('Lucy');
+console.log(lucy.hello());
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,107 +1,100 @@
-import React, {useEffect} from 'react';
-import Person from '../components/decorator'
+import React, { useEffect } from 'react';
+import Person from '../components/decorator';
 
-import {Button} from 'antd'
-import {connect} from 'dva'
+import { Button } from 'antd';
+import { connect } from 'dva';
 import styles from './index.less';
 
 //generic types
 interface GenericFunc {
-  <Q>(arg: Q): Q
+  <Q>(arg: Q): Q;
 }
 
 // 提升
 interface GenericFuncAll<W> {
-  (arg: W): W
+  (arg: W): W;
 }
 
 //generic class
 class GenericClass<T> {
   one: number = 0;
-  add: (a: T, b: T) => T = (a, b) => a
+  add: (a: T, b: T) => T = (a, b) => a;
 }
 
 //extends
 //keyof
 
 class MyClass {
-  [s: string]: boolean | ((s: string) => boolean)
+  [s: string]: boolean | ((s: string) => boolean);
 
   s = (a: string) => true;
   check(a: string) {
-    return this[a] as boolean
+    return this[a] as boolean;
   }
 }
 
-
-export default connect(({home})=>({home}))((props) => {
-  console.log('home', props.home)
-  console.log('sha', new MyClass().check('s'))
+export default connect(({ home }) => ({ home }))((props) => {
+  console.log('home', props.home);
+  console.log('sha', new MyClass().check('s'));
   // useEffect(() => {
   //   props.dispatch({
   //     type: 'home/fetchUser'
   //   })
   // }, [props.dispatch])
-  console.log('-', Person.isTestable)
-  const p1 = new Person('leehow')
-  p1.name = 'ddd'
-  console.log('p1 name', p1.name)
+  console.log('-', Person.isTestable);
+  const p1 = new Person('leehow');
+  p1.name = 'ddd';
+  p1.sayHi();
+  console.log('p1 name', p1.name);
 
   const instance = new GenericClass<number>();
   instance.one = 1;
   instance.add = (a, b) => a + b;
 
-
   // ts playground
   const d: object = {
-    a: ''
-  }
+    a: '',
+  };
   d;
 
   function identity<T>(arg: T): T {
-    return arg
+    return arg;
   }
-
 
   function a<T>(arg: T[]): T[] {
-    return arg
+    return arg;
   }
 
-  const b: number[] = [1, 2, 3]
+  const b: number[] = [1, 2, 3];
 
-  let func: <T>(arg: T) => T = identity
+  let func: <T>(arg: T) => T = identity;
 
-  let func1: { <S>(arg: S): S } = identity
-  let func2: GenericFunc = identity
+  let func1: { <S>(arg: S): S } = identity;
+  let func2: GenericFunc = identity;
 
-  identity(2)
-
-
+  identity(2);
 
   interface Point {
     x: number;
     y: number;
   }
 
-  function aaaaa(x: unknown) {
-
-  }
+  function aaaaa(x: unknown) {}
 
   type Predicate = (x: unknown) => boolean;
   type K = ReturnType<Predicate>;
 
-  type P = keyof Point
-
+  type P = keyof Point;
 
   interface Animal {
-    live:() => void;
+    live: () => void;
     live1(): void;
   }
 
   type Animal1 = {
     a(): void;
     b(): void;
-  }
+  };
 
   // const h1Ref = React.useRef<HTMLHeadingElement | null>(null)
   // (h1Ref.current as HTMLHeadingElement)
@@ -112,4 +105,4 @@ export default connect(({home})=>({home}))((props) => {
       <Button type="primary">Primary Button</Button>
     </div>
   );
-})
+});
